a8afa34467d7b5fda4940562db648af4fdcb255d
Metaldetect <stas.klymenko@icloud.com>
2023-05-14 09:31:56 +0200

    Change

--- js/task-04.js
+++ js/task-04.js
@@ -2,7 +2,7 @@ const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const valueEl = document.querySelector("#value");
 
-let counterValue = 0;
+let counterValue = 1;
 
 decrementBtn.addEventListener("click", () => {
   counterValue -= 1;
